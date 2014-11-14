34b6168c81a84b32b66a208755fcaa0bac7a5573
Rock Zhang <iamgtskk@gmail.com>
2014-11-14 17:52:54 +0800

    log4js模块、404状态码添加,具体修改见README文件

--- app.js
+++ app.js
@@ -3,27 +3,15 @@
  */
 
 var express = require('express');
-var morgan = require('morgan');
-var fs = require('fs');
-
-// var http = require('http');
 var routes = require('./routes');
 var config = require('./config.js').config;
 var partials = require('express-partials');
+var util = require('./lib/util');
 
 var app = express();
 var static_dir = __dirname + '/public';
 var theme_static_dir = __dirname + '/views/theme/' + config.theme + '/assets';
 var admin_static_dir = __dirname + '/views/admin/assets';
-
-// create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(__dirname + '/error.log', {flags: 'a'});
-app.use(morgan('short', {
-  stream: accessLogStream,
-  skip: function(req, res){ // 只记录错误和跳转响应
-    return res.statusCode < 300 || res.statusCode == 304;
-  }
-}));
 
 app.configure(function () {
   app.set('port', config.port);
@@ -32,7 +20,9 @@ app.configure(function () {
   app.use(express.compress());
   app.use(partials());
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
-  app.use(express.logger('dev'));
+  //app.use(express.logger('dev'));
+  // 采用morgan
+  app.use(util.httplogger);
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: config.session_secret }));
